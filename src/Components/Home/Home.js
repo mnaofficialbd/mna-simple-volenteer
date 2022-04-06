53f7b50a21c6dd7115ffaeae72212ce94d069505
mnaofficial <mna.official92@gmail.com>
2022-04-06 12:21:35 +0600

    main

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
+    const [count,setCount]=useState(0);
+    const number=56;
+    const increaseCount=()=>{
+        setCount(count+1)
+    }
     return (
         <div>
             <h2>This is Home</h2>
+            <button onClick={increaseCount}>Click</button>
+            <p>Count: {count}</p>
+            <p>My Number: {number}</p>
+            <p>Count: {count}</p>
+            <p>Double Count: {count*2}</p>
         </div>
     );
 };
