e672e2d700bcbcd36c57c5ee66cfdae0dcf0e4b3
mnaofficial <mna.official92@gmail.com>
2022-04-06 19:34:35 +0600

    data

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import useVolunteers from '../../hooks/useVolunteers';
 import Activity from '../Activity/Activity';
 import "./Main.css"
 
 const Main = () => {
-    const [volunteers] = useVolunteers();
+    // const [volunteers] = useVolunteers();
+    const [searchText,setSearchText]=useState('')
+    const [searchResult,setSearchResult]=useState([])
+
+    useEffect(()=>{
+        fetch('data.json')
+        .then(res => res.json())
+        .then(data => {
+            const match=data.filter(d=>d.title.includes(searchText));
+            setSearchResult(match)
+        })
+    },[searchText])
 
     const handleSearchChange = (event) => {
-        console.log(event.target.value);
+        setSearchText(event.target.value);
+        
     }
+
+    /* const handleSearchChange = (event) => {
+        const searchText=event.target.value;
+        const match=volunteers.filter(v=>v.title.includes(searchText))
+        setSearchResult(match)
+    } */
+
+
     return (
         <div>
-            <h4>Volunteers Activities: {volunteers.length}</h4>
+            <h4>Volunteers Activities: {searchResult.length}</h4>
             <div style={{ 'margin': '20px' }}>
                 <input onChange={handleSearchChange} type="text" placeholder='Search' />
             </div>
             <div className='activity-container'>
                 {
-                    volunteers.map(activity => <Activity
+                    searchResult.map(activity => <Activity
                         key={activity.taskId}
                         activity={activity}
                     ></Activity>)
